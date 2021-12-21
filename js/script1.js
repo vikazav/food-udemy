7c9a0bf37fb1ed5c0bce13c9ed5c4c2e199d9e97
Victoria <vikvv@ukr.net>
2021-12-21 16:11:07 +0200

    second version getResource function

--- js/script1.js
+++ js/script1.js
@@ -166,6 +166,7 @@ this.classes.forEach(className => element.classList.add(className));
                 </div>
             
     `;
+    
 this.parent.append(element);
     }
 }
@@ -183,9 +184,26 @@ getResource('http://localhost:3000/menu')
 new MenuCard(img, alt, title, descr, price, '.menu .container').render();
     });
 });
+// getResource('http://localhost:3000/menu')
+// .then(data => createCard(data)); 
 
-
-
+// function createCard(data) {
+//     data.forEach(({img, alt, title, descr, price})=> {
+//     const element = document.createElement('div');
+//     element.classList.add('menu__item');
+//     element.innerHTML =`
+//     <img src=${img} alt=${alt}>
+//     <h3 class="menu__item-subtitle">${title}</h3>
+//     <div class="menu__item-descr">${descr}</div>
+//     <div class="menu__item-divider"></div>
+//     <div class="menu__item-price">
+//         <div class="menu__item-cost">Цена:</div>
+//         <div class="menu__item-total"><span>${price}</span> грн/день</div>
+//     </div>
+// `;
+// document.querySelector('.menu .container').append(element);
+//     });
+// }
 
 //  FORMS
 
